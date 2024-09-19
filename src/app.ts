13f46775bda48a5ec1a8a9ab5d3849901bdee521
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-09-19 21:02:48 +0600

    Added necessary middlewares and initialized app.ts and server.ts

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import router from "./app/routes";
+import globalErrorHandler from "./app/middleWears/globalErrorHandler";
+import notFound from "./app/middleWears/notFound";
+
+const app: Application = express();
+app.use(cors());
+// app.use(cookieParser());
+
+// Parsers / MiddleWears
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", router);
+app.use(globalErrorHandler);
+app.use(notFound);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "Arkham healthcare Server",
+  });
+});
+
+export default app;
